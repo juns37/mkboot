47845fe13e2c3ba31cf69b9f0da1a8b727f402fc
juns37 <zhune37.0@gmail.com>
2026-02-22 11:41:55 +0900

    Update setup and readme..

--- README.md
+++ README.md
@@ -1,5 +1,28 @@
-Clone repo ini
+#Install dependensi untuk MKBOOT...
 
+## Update repository
+```
+pkg update && pkg upgrade -y
+```
+
+## Install semua tool yang dibutuhkan script
+• fzf: untuk menu interaktif
+• ffmpeg: untuk olah video
+• imagemagick: untuk olah gambar (magick)
+• zip: untuk membuat file bootanimation.zip
+```
+pkg install python git fzf ffmpeg imagemagick zip -y
+```
+
+## Memberikan izin akses ke penyimpanan internal (PENTING untuk /sdcard/boot)
+```
+termux-setup-storage
+```
+
+## Clone repo ini
 ```
 git clone https://github.com/juns37/mkboot.git && cd mkboot && bash setup.sh
 ```
+
+[Group Support](http://t.me/mengpoi);
+
--- setup.sh
+++ setup.sh
@@ -1,25 +1,10 @@
 #!/bin/bash
 
-echo "Menginstall dependensi untuk MKBOOT..."
-
-# Update repository
-pkg update && pkg upgrade -y
-
-# Install semua tool yang dibutuhkan script
-# fzf: untuk menu interaktif
-# ffmpeg: untuk olah video
-# imagemagick: untuk olah gambar (magick)
-# zip: untuk membuat file bootanimation.zip
-pkg install python git fzf ffmpeg imagemagick zip -y
-
-# Memberikan izin akses ke penyimpanan internal (PENTING untuk /sdcard/boot)
-termux-setup-storage
-
 # Beri izin eksekusi pada script utama
-chmod +x mkboot
+chmod +x bin/mkboot
 
 # Pindahkan ke bin agar bisa dipanggil langsung
-cp mkboot $PREFIX/bin/
+cp bin/mkboot $PREFIX/bin/
 
 echo "----------------------------------------"
 echo "✅ Instalasi Selesai!"
